9eb00df64bc0f50b5647c7e299c339e717fa8230
ThomasFinnern <ThomasFinnern@users.noreply.github.com>
2025-03-24 15:28:24 +0100

    call build batches / *.tsk

---
+++ .buildPHP/build_develop.bat
@@ -0,0 +1,41 @@
+@ECHO OFF
+REM build_develop.bat
+REM
+CLS
+
+REM Path for calling
+set ExePath=C:\Program Files\php82\
+REM ECHO ExePath: "%ExePath%"
+
+if exist "%ExePath%php.exe" (
+    REM path known (WT)
+    ECHO ExePath: "%ExePath%"
+) else (
+    REM Direct call
+    ECHO PHP in path variable
+    set ExePath=
+)
+
+REM "C:\Program Files\php82\php.exe" --version
+"%ExePath%php.exe" --version
+
+ECHO ----------------------------------------------
+ECHO.
+
+pushd  ..\..\buildExtension\src
+REM dir /one /b *.tsk
+ECHO Path: %cd% 
+
+echo --- "%ExePath%php.exe" buildExtensionCmd.php -f ../../remoteMarkdown/.buildPHP/build_develop.tsk %1
+"%ExePath%php.exe" buildExtensionCmd.php -f ../../remoteMarkdown/.buildPHP/build_develop.tsk %1
+popd
+
+goto :EOF
+
+REM ------------------------------------------
+REM Adds given argument to the already known command arguments
+:AddNextArg
+    Set NextArg=%*
+    Set CmdArgs=%CmdArgs% %NextArg%
+    ECHO  '%NextArg%'
+GOTO :EOF
