9f6dd4542b218c70958c0984b9b2d9905493bfac
ThomasFinnern <ThomasFinnern@users.noreply.github.com>
2025-06-29 16:53:41 +0200

    Renamed build.bat and inside *.tsk for changed bade folder name

--- .buildPHP/build.bat
+++ .buildPHP/build.bat
@@ -22,12 +22,37 @@ REM "C:\Program Files\php82\php.exe" --version
 ECHO ----------------------------------------------
 ECHO.
 
+set OptionFile=
+
+if %1A==-dA (
+	set OptionFile=-o options_version_tsk\build_develop.opt
+)
+
+if %1A==-sA (
+	set OptionFile=-o options_version_tsk\build_step.opt
+)
+
+if %1A==-fA (
+	set OptionFile=-o options_version_tsk\build_fix.opt
+)
+
+if %1A==-rA (
+	set OptionFile=-o options_version_tsk\build_release.opt
+)
+
+if %1A==-mA (
+	set OptionFile=-o options_version_tsk\build_major.opt
+)
+
+ECHO ----------------------------------------------
+ECHO.
+
 pushd  ..\..\buildExtension\src
 REM dir /one /b *.tsk
 ECHO Path: %cd% 
 
-echo --- "%ExePath%php.exe" buildExtensionCmd.php -f ../../remoteMarkdown/.buildPHP/build.tsk %1
-"%ExePath%php.exe" buildExtensionCmd.php -f ../../remoteMarkdown/.buildPHP/build.tsk %1
+echo --- "%ExePath%php.exe" buildExtensionCmd.php -f ../../plg_remoteMarkdown/.buildPHP/build.tsk %1 %OptionFile%
+"%ExePath%php.exe" buildExtensionCmd.php -f ../../plg_remoteMarkdown/.buildPHP/build.tsk %1 %OptionFile%
 popd
 
 GOTO :EOF
